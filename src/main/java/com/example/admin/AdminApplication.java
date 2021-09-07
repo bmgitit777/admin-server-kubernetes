b28b89b5736f6469315bf2e55d08da3ca2359666
bmgitit777 <mark.berry_rtk@yahoo.com>
2021-09-07 16:28:30 -0700

    update image, add build-info & @EnableScheduling

--- src/main/java/com/example/admin/AdminApplication.java
+++ src/main/java/com/example/admin/AdminApplication.java
@@ -1,13 +1,17 @@
 package com.example.admin;
 
+//@EnableScheduling forces the discovery client to periodically call the Kubernetes API to refresh the list of running services.
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableScheduling
 public class AdminApplication {
 
 	public static void main(String[] args) {
